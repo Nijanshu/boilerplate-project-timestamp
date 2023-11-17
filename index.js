137eab0a9b15111b8ed36ff82cb45ba18cc05585
Nijanshu <46223306+Nijanshu@users.noreply.github.com>
2023-11-17 21:28:14 +0530

    Done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+require('dotenv').config()
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -22,6 +23,16 @@ app.get("/", function (req, res) {
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
+});
+
+app.get("/api/:date", function (req, res) {
+  let reqq=req.params.date
+  let arr=reqq.split('-')
+  let dat=new Date(arr[0],arr[1],arr[2]).toString()
+  res.json({
+    unix:Date.parse(reqq),
+    utc:dat.split('+')[0]
+  });
 });
 
 
